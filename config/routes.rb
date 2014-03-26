c6d87e18b5d5cd9dbeab13d3eca3928b53bffb8b
vagrant <vagrant@kellogg-dev-box>
2014-03-26 22:03:41 +0000

    Add a new Book model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Homework::Application.routes.draw do
+
+  get "/books/list"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
